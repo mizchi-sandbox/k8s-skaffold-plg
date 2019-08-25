cacb05fe924bad4e90028848dde6c671eb11dc10
mizchi <miz404@gmail.com>
2019-08-25 11:15:37 +0900

    It works

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+io.on("connection", socket => {
+  console.log("a user connected", socket.id);
+  socket.broadcast.emit(`user connected`);
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
