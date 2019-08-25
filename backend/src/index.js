d0e2bb0b774b6572fdc79eedd294cf0d86ffe54f
mizchi <miz404@gmail.com>
2019-08-25 09:25:33 +0900

    Init

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { echo } = require("./utils");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send(echo("Hello World!")));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+console.log("server started!", JSON.stringify(process.env, null, 2));
